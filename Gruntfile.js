5573f7364d45db55cb2cf1bd023ecee2ffe85ed5
ronky <martin.ronky@gmail.com>
2014-06-30 20:28:21 +0200

    fixed grunt watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -153,7 +153,7 @@ module.exports = function (grunt) {
             }
         },
         watch: {
-            build: {
+            hint: {
                 files: ['src/**/*'],
                 tasks: ['build']
             }
